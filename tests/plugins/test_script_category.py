6b79421348e06c2c146f72748e98c7e5f25a6a26
Björn Ricks <bjoern.ricks@greenbone.net>
2022-02-08 10:11:17 +0100

    Add new plugin for checking the value of the script_category function
    This plugin only supports a part of the original check script. The
    original check script also considered dependencies on other scripts.

---
+++ tests/plugins/test_script_category.py
@@ -0,0 +1,56 @@
+# Copyright (C) 2022 Greenbone Networks GmbH
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from pathlib import Path
+
+import unittest
+
+from naslinter.plugin import LinterError
+from naslinter.plugins.script_category import CheckScriptCategory
+
+
+class CheckDependencyCategoryOrderTestCase(unittest.TestCase):
+    def test_ok(self):
+        path = Path("some/file.nasl")
+        content = "script_category(ACT_GATHER_INFO);"
+
+        results = list(CheckScriptCategory.run(path, content))
+        self.assertEqual(len(results), 0)
+
+    def test_missing_category(self):
+        path = Path("some/file.nasl")
+        content = ""
+
+        results = list(CheckScriptCategory.run(path, content))
+        self.assertEqual(len(results), 1)
+        self.assertIsInstance(results[0], LinterError)
+        self.assertEqual(
+            "VT 'some/file.nasl' is missing a script_category.",
+            results[0].message,
+        )
+
+    def test_invalid_category(self):
+        path = Path("some/file.nasl")
+        content = "script_category(ACT_FOO);"
+
+        results = list(CheckScriptCategory.run(path, content))
+        self.assertEqual(len(results), 1)
+        self.assertIsInstance(results[0], LinterError)
+        self.assertEqual(
+            "VT 'some/file.nasl' is using an unsupported category 'ACT_FOO'.",
+            results[0].message,
+        )
