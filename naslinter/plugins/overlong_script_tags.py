3acc3836a3d15ab290f4b601dceb5d79bf258811
Simonas Simenas <simonas.simenas@greenbone.net>
2022-02-09 13:14:58 +0100

    Added plugin to check overlong script_tags.

---
+++ naslinter/plugins/overlong_script_tags.py
@@ -0,0 +1,80 @@
+# Copyright (C) 2021 Greenbone Networks GmbH
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import re
+
+from pathlib import Path
+from typing import Iterable
+
+from ..plugin import LinterError, LineContentPlugin
+
+# Arbitrary limit adopted from original step
+VALUE_LIMIT = 3000
+
+
+class CheckOverlongScriptTags(LineContentPlugin):
+    """This steps checks if the script_tag summary, impact,
+    affected, insight, vuldetect or solutionof a given VT
+    contains an overlong line within the value string.
+
+    Background for this is that e.g. auto generated LSCs
+    where are created by parsing an advisory and the whole
+    content is placed in such a tag which could be quite large.
+    """
+
+    name = "check_overlong_script_tags"
+
+    @staticmethod
+    def run(nasl_file: Path, lines: Iterable[str]):
+        report = ""
+        # Only applies to .nasl files but not to .inc
+        if nasl_file.suffix != ".nasl":
+            return
+
+        ignore_files = [
+            "gb_nmap6_",
+            "monstra_cms_mult_vuln",
+            "gb_huawei-sa-",
+            "lsc_options.nasl",
+        ]
+
+        if any(ignore in nasl_file.name for ignore in ignore_files):
+            return
+
+        line_number = 0
+        for line in lines:
+            # Length of value to check is found in group 3
+            # Tag name is found in group 2
+            expre = re.search(
+                r"(script_tag\(\s*name\s*:\s*\""
+                r"(summary|impact|affected|insight|vuldetect|solution)"
+                r"\"\s*,\s*value\s*:\s*)(\"[^\"]+\")\)",
+                line,
+            )
+            if expre is not None:
+                if len(expre.group(3)) > VALUE_LIMIT:
+                    report += (
+                        f"line {line_number : 5}:"
+                        f" contains overlong {expre.group(2)}"
+                        f" with {len(expre.group(3))} characters"
+                    )
+            line_number += 1
+
+        if len(report) > 0:
+            yield LinterError(
+                f"VT {str(nasl_file)} with limit of {VALUE_LIMIT}: \n{report}\n"
+            )
