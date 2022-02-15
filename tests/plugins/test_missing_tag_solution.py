cbfd701a1b3f69460b06555a469a362b0cf675b3
Jaspar Löchte <jaspar.loechte@greenbone.net>
2022-02-15 15:17:11 +0100

    Add tests for missing solution tag

---
+++ tests/plugins/test_missing_tag_solution.py
@@ -0,0 +1,69 @@
+# Copyright (C) 2022 Greenbone Networks GmbH
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from pathlib import Path
+
+import unittest
+
+from naslinter.plugin import LinterError, LinterResult
+from naslinter.plugins.missing_tag_solution import CheckMissingTagSolution
+
+
+class CheckDoubleEndPointsTestCase(unittest.TestCase):
+    def test_ok(self):
+        path = Path("some/file.nasl")
+        content = (
+            'script_tag(name:"cvss_base", value:"4.0");\n'
+            'script_tag(name:"summary", value:"Foo Bar.");\n'
+            'script_tag(name:"solution_type", value:"VendorFix");\n'
+            'script_tag(name:"solution", value:"meh");\n'
+        )
+
+        results = list(CheckMissingTagSolution.run(path, content))
+        self.assertEqual(len(results), 0)
+
+    def test_no_solution_type(self):
+        path = Path("some/file.nasl")
+        content = (
+            'script_tag(name:"cvss_base", value:"4.0");\n'
+            'script_tag(name:"summary", value:"Foo Bar...");'
+        )
+
+        results = list(CheckMissingTagSolution.run(path, content))
+        self.assertEqual(len(results), 1)
+        self.assertIsInstance(results[0], LinterResult)
+        self.assertEqual(
+            "Nothing to do here.",
+            results[0].message,
+        )
+
+    def test_missing_solution(self):
+        path = Path("some/file.nasl")
+        content = (
+            'script_tag(name:"cvss_base", value:"4.0");\n'
+            'script_tag(name:"summary", value:"Foo Bar...");'
+            'script_tag(name:"solution_type", value:"VendorFix");\n'
+        )
+
+        results = list(CheckMissingTagSolution.run(path, content))
+        self.assertEqual(len(results), 1)
+        self.assertIsInstance(results[0], LinterError)
+        self.assertEqual(
+            "'solution_type' script_tag but no 'solution' script_tag "
+            f"found in the description block of VT '{str(path)}'",
+            results[0].message,
+        )
