89d790c8ab8779c35c8bd5dcc537bb28a9332357
Jaspar Löchte <jaspar.loechte@greenbone.net>
2022-02-18 23:13:39 +0100

    Add CheckTabs replacing tabs, and test

---
+++ naslinter/plugins/tabs.py
@@ -0,0 +1,39 @@
+# Copyright (C) 2021 Greenbone Networks GmbH
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from pathlib import Path
+from typing import Iterator
+
+from naslinter.plugin import LinterWarning, FileContentPlugin, LinterResult
+
+TAB_TO_SPACES = 2
+
+
+class CheckTabs(FileContentPlugin):
+    name = "check_tabs"
+
+    @staticmethod
+    def run(nasl_file: Path, file_content: str) -> Iterator[LinterResult]:
+        """This script checks if a VT is using one or
+        more tabs instead of spaces."""
+
+        if "\t" in file_content:
+            file_content = file_content.replace("\t", " " * TAB_TO_SPACES)
+            nasl_file.write_text(data=file_content, encoding="latin1")
+            yield LinterWarning(
+                f"Replaced one or more tabs to {TAB_TO_SPACES} spaces!"
+            )
---
+++ tests/plugins/test_tabs.py
@@ -0,0 +1,61 @@
+# Copyright (C) 2021 Greenbone Networks GmbH
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from pathlib import Path
+
+import unittest
+
+from naslinter.plugin import LinterWarning
+from naslinter.plugins.tabs import CheckTabs, TAB_TO_SPACES
+
+
+class CheckTabsTestCase(unittest.TestCase):
+    def test_ok(self):
+        path = Path("tests/file.nasl")
+        content = "What ever."
+
+        results = list(CheckTabs.run(path, content))
+
+        self.assertEqual(len(results), 0)
+
+    def test_with_tabs(self):
+        path = Path("tests/file.nasl")
+
+        path.write_text(
+            "\t\t\t\t\t\t\n1234456789",
+            encoding="utf-8",
+        )
+        content = path.read_text(encoding="latin1")
+
+        expected_content = "            \n1234456789"
+
+        results = list(CheckTabs.run(path, content))
+        self.assertEqual(len(results), 1)
+        print(results)
+
+        self.assertIsInstance(results[0], LinterWarning)
+        self.assertEqual(
+            f"Replaced one or more tabs to {TAB_TO_SPACES} spaces!",
+            results[0].message,
+        )
+        self.assertEqual(
+            path.read_text(encoding="latin1"),
+            expected_content,
+        )
+
+        if path.exists():
+            path.unlink()
