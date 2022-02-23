6236e3c49cc4e600b247e4ccf45f9221a744d883
Josef Fröhle <josef.froehle@greenbone.net>
2022-02-23 11:49:58 +0100

    add suggestions

--- naslinter/plugins/http_links_in_tags.py
+++ naslinter/plugins/http_links_in_tags.py
@@ -18,7 +18,7 @@
 import re
 
 from pathlib import Path
-from typing import Iterator
+from typing import Iterator, AnyStr
 
 from itertools import chain
 from naslinter.plugin import LinterError, FileContentPlugin, LinterResult
@@ -72,121 +72,12 @@ class CheckHttpLinksInTags(FileContentPlugin):
                     )
                     if http_link_matches is not None:
                         for http_link_match in http_link_matches:
-                            if (
-                                http_link_match is not None
-                                and http_link_match.group(1) is not None
-                            ):
-                                if (
-                                    "The payloads try to open a connection to "
-                                    "www.google.com" in http_link_match.group(0)
+                            if http_link_match:
+                                if CheckHttpLinksInTags.check_to_continue(
+                                    http_link_match.group(0)
                                 ):
                                     continue
-                                if (
-                                    "The script attempts to connect to www."
-                                    "google.com" in http_link_match.group(0)
-                                ):
-                                    continue
-                                if (
-                                    "to retrieve a web page from www.google"
-                                    ".com" in http_link_match.group(0)
-                                ):
-                                    continue
-                                if (
-                                    "Subject: commonName=www.paypal.com"
-                                    in http_link_match.group(0)
-                                ):
-                                    continue
-                                if (
-                                    "Terms of use at https://www.veris"
-                                    "ign.com/rpa" in http_link_match.group(0)
-                                ):
-                                    continue
-                                if "example.com" in http_link_match.group(
-                                    0
-                                ) or "example.org" in http_link_match.group(0):
-                                    continue
-                                if "www.exam" in http_link_match.group(0):
-                                    continue
-                                if (
-                                    "sampling the resolution of a name "
-                                    "(www.google.com)"
-                                    in http_link_match.group(0)
-                                ):
-                                    continue
-                                if (
-                                    "once with 'www.' and once without"
-                                    in http_link_match.group(0)
-                                ):
-                                    continue
-                                if (
-                                    "wget http://www.javaop.com/~ron/tmp/nc"
-                                    in http_link_match.group(0)
-                                ):
-                                    continue
-                                if (
-                                    "Ncat: Version 5.30BETA1 (http://nmap.org/"
-                                    "ncat)" in http_link_match.group(0)
-                                ):
-                                    continue
-                                if (
-                                    "as www.windowsupdate.com. (BZ#506016)"
-                                    in http_link_match.group(0)
-                                ):
-                                    continue
-                                if (
-                                    "located at http://sambarserver/session/p"
-                                    "agecount." in http_link_match.group(0)
-                                ):
-                                    continue
-                                if (
-                                    "http://rest.modx.com"
-                                    in http_link_match.group(0)
-                                ):
-                                    continue
-                                if (
-                                    "ftp:// " in http_link_match.group(0)
-                                    or "ftp://'" in http_link_match.group(0)
-                                    or "ftp://)" in http_link_match.group(0)
-                                    or "ftp.c" in http_link_match.group(0)
-                                    or "ftp.exe" in http_link_match.group(0)
-                                ):
-                                    continue
-                                if (
-                                    "using special ftp://"
-                                    in http_link_match.group(0)
-                                    or "running ftp."
-                                    in http_link_match.group(0)
-                                    or "ftp. The vulnerability"
-                                    in http_link_match.group(0)
-                                ):
-                                    continue
-                                if (
-                                    "'http://' protocol"
-                                    in http_link_match.group(0)
-                                    or "handle <a href='http://...'> pro"
-                                    "perly" in http_link_match.group(0)
-                                ):
-                                    continue
-                                if (
-                                    "Switch to git+https://"
-                                    in http_link_match.group(0)
-                                ):
-                                    continue
-                                if (
-                                    "wget https://compromised-domain.com/impo"
-                                    "rtant-file" in http_link_match.group(0)
-                                ):
-                                    continue
-                                if (
-                                    "the https:// scheme"
-                                    in http_link_match.group(0)
-                                ):
-                                    continue
-                                if (
-                                    "https://www.phishingtarget.com@evil.com"
-                                    in http_link_match.group(0)
-                                ):
-                                    continue
+
                                 http_link_tag = (
                                     "\n\t"
                                     + tag_match.group(0).partition(",")[0]
@@ -201,9 +92,6 @@ class CheckHttpLinksInTags(FileContentPlugin):
                                     '\'script_xref(name:"URL", value:"");\' '
                                     f"tag instead:{http_link_tag}"
                                 )
-            return
-
-        return
 
     @staticmethod
     def contains_nvd_mitre_link_in_xref(
@@ -225,11 +113,6 @@ class CheckHttpLinksInTags(FileContentPlugin):
                 file_content: The content of the file that is going to be
                                 checked
         """
-        # Does only apply to NASL files.
-        if not nasl_file.suffix == ".nasl":
-            return
-
-        nasl_file_str = str(nasl_file)
 
         tag_matches = re.finditer(
             r'(script_xref\(name\s*:\s*"URL"\s*,\s*value\s*:\s*")([^"]+)"',
@@ -238,8 +121,7 @@ class CheckHttpLinksInTags(FileContentPlugin):
 
         if tag_matches is not None:
             for match in tag_matches:
-                if match is not None and match.group(2) is not None:
-
+                if match:
                     if (
                         # fmt: off
                         "nvd.nist.gov/vuln/detail/CVE-" in match.group(2) \
@@ -250,11 +132,95 @@ class CheckHttpLinksInTags(FileContentPlugin):
                         nvd_mitre_link_tag = "\n\t" + match.group(0)
                         yield LinterError(
                             "The following script_xref of VT "
-                            f"'{nasl_file_str}' is pointing to Mitre/NVD "
+                            f"'{nasl_file}' is pointing to Mitre/NVD "
                             "which is already covered by the script_cve_id. "
                             "This is a redundant info and the script_xref "
                             f"needs to be removed: {nvd_mitre_link_tag}"
                         )
-            return
 
-        return
+    @staticmethod
+    def check_to_continue(http_link_match_group: AnyStr) -> bool:
+        if (
+            "The payloads try to open a connection to www.google.com"
+            in http_link_match_group
+        ):
+            return True
+        if (
+            "The script attempts to connect to www.google.com"
+            in http_link_match_group
+        ):
+            return True
+        if (
+            "to retrieve a web page from www.google.com"
+            in http_link_match_group
+        ):
+            return True
+        if "Subject: commonName=www.paypal.com" in http_link_match_group:
+            return True
+        if (
+            "Terms of use at https://www.verisign.com/rpa"
+            in http_link_match_group
+        ):
+            return True
+        if (
+            "example.com" in http_link_match_group
+            or "example.org" in http_link_match_group
+        ):
+            return True
+        if "www.exam" in http_link_match_group:
+            return True
+        if (
+            "sampling the resolution of a name (www.google.com)"
+            in http_link_match_group
+        ):
+            return True
+        if "once with 'www.' and once without" in http_link_match_group:
+            return True
+        if "wget http://www.javaop.com/~ron/tmp/nc" in http_link_match_group:
+            return True
+        if (
+            "Ncat: Version 5.30BETA1 (http://nmap.org/ncat)"
+            in http_link_match_group
+        ):
+            return True
+        if "as www.windowsupdate.com. (BZ#506016)" in http_link_match_group:
+            return True
+        if (
+            "located at http://sambarserver/session/pagecount."
+            in http_link_match_group
+        ):
+            return True
+        if "http://rest.modx.com" in http_link_match_group:
+            return True
+        if (
+            "ftp:// " in http_link_match_group
+            or "ftp://'" in http_link_match_group
+            or "ftp://)" in http_link_match_group
+            or "ftp.c" in http_link_match_group
+            or "ftp.exe" in http_link_match_group
+        ):
+            return True
+        if (
+            "using special ftp://" in http_link_match_group
+            or "running ftp." in http_link_match_group
+            or "ftp. The vulnerability" in http_link_match_group
+        ):
+            return True
+        if (
+            "'http://' protocol" in http_link_match_group
+            or "handle <a href='http://...'> properly" in http_link_match_group
+        ):
+            return True
+        if "Switch to git+https://" in http_link_match_group:
+            return True
+        if (
+            "wget https://compromised-domain.com/important-file"
+            in http_link_match_group
+        ):
+            return True
+        if "the https:// scheme" in http_link_match_group:
+            return True
+        if "https://www.phishingtarget.com@evil.com" in http_link_match_group:
+            return True
+
+        return False
