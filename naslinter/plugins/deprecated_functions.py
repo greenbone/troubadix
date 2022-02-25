e64b430a671811a46fe3514144e29fc83549aa0a
Jaspar Löchte <jaspar.loechte@greenbone.net>
2022-02-25 10:33:34 +0100

    Finalize DeprecatedFunctions Plugin

--- naslinter/plugins/deprecated_functions.py
+++ naslinter/plugins/deprecated_functions.py
@@ -1,73 +1,69 @@
-#!/usr/bin/env python3
+# Copyright (C) 2022 Greenbone Networks GmbH
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# pylint: disable=fixme
 
 import re
-import sys
+
+from pathlib import Path
+from typing import Iterator
+
+from naslinter.plugin import (
+    LinterError,
+    FileContentPlugin,
+    LinterResult,
+)
 
 
-def is_using_only_current_functions(file):
-    """
-    Following functions / description items are outdated:
-    script_summary()
-    script_id()
-    security_note()
-    security_warning()
-    security_hole()
-    script_description()
-    script_tag("risk_factor", SEVERITY);
+class CheckDeprecatedFunctions(FileContentPlugin):
+    name = "check_deprecated_functions"
 
-    This script checks if any of those are used
+    @staticmethod
+    def run(nasl_file: Path, file_content: str) -> Iterator[LinterResult]:
+        """
+        Following functions / description items are outdated:
+        script_summary()
+        script_id()
+        security_note()
+        security_warning()
+        security_hole()
+        script_description()
+        script_tag("risk_factor", SEVERITY);
 
-    Args:
-        file: Name of the VT to be checked
+        This script checks if any of those are used
 
-    Returns:
-        tuples: 0 => Success, no message
-               -1 => Error, with error message
+        Args:
+            nasl_file: Name of the VT to be checked
+        """
+        deprecated_functions = {
+            "script_summary(), use script_tag"
+            '(name:"summary", value:"") instead': r"script_summary\s*\([^)]*\)",
+            "script_id(), use script_oid() with "
+            "the full OID instead": r"script_id\s*\([0-9]+\)",
+            "security_note()": r"security_note\s*\([^)]*\)",
+            "security_warning()": r"security_warning\s*\([^)]*\)",
+            "security_hole()": r"security_hole\s*\([^)]*\)",
+            "script_description()": r"script_description\s*\([^)]*\)",
+            'script_tag(name:"risk_factor", value: '
+            "SEVERITY)": r'script_tag\s*\(\s*name:\s*"risk_factor"[^)]*\)',
+        }
 
-    """
-
-    text = open(file, encoding="latin-1").read()
-
-    deprecated_functions = {
-        'script_summary(), use script_tag(name:"summary", value:"") instead': "script_summary\s*\([^)]*\)",
-        "script_id(), use script_oid() with the full OID instead": "script_id\s*\([0-9]+\)",
-        "security_note()": "security_note\s*\([^)]*\)",
-        "security_warning()": "security_warning\s*\([^)]*\)",
-        "security_hole()": "security_hole\s*\([^)]*\)",
-        "script_description()": "script_description\s*\([^)]*\)",
-        'script_tag(name:"risk_factor", value: SEVERITY)': 'script_tag\s*\(\s*name:\s*"risk_factor"[^)]*\)',
-    }
-
-    functions_used = ""
-
-    for key in deprecated_functions:
-        if re.search(deprecated_functions[key], text):
-            functions_used += "\n\t" + key
-
-    if len(functions_used) > 0:
-        return -1, "The following functions of VT '" + str(
-            file
-        ) + "' are deprecated:" + str(functions_used)
-
-    else:
-        return (0,)
-
-
-if __name__ == "__main__":
-    import ci_helpers
-
-    error = []
-    files = ci_helpers.list_modified_files()
-    if files:
-        for file in files:
-            test = is_using_only_current_functions(file)
-            if test[0] != 0:
-                error.append(file)
-    else:
-        sys.exit(0)
-
-    if len(error) > 0:
-        ci_helpers.report("VTs using deprecated functions", error)
-        sys.exit(1)
-
-    sys.exit(0)
+        for description, pattern in deprecated_functions.items():
+            if re.search(pattern, file_content):
+                yield LinterError(
+                    f"Found a deprecated function call: {description}"
+                )
