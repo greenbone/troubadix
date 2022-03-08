a90b7955b38514264c8043003c3c98f0ca06fc74
pascalholthaus <pascal.holthaus@greenbone.net>
2022-03-08 11:19:15 +0100

    Add some new lines

--- tests/plugins/test_description.py
+++ tests/plugins/test_description.py
@@ -51,6 +51,7 @@ class CheckDescriptionTestCase(PluginTestCase):
             'script_tag(name:"solution", value:"meh");\n'
             'script_description("TestTest");\n'
         ).splitlines()
+
         results = list(
             CheckDescription.run(
                 nasl_file,
@@ -76,6 +77,7 @@ class CheckDescriptionTestCase(PluginTestCase):
             'script_cve_id("CVE-2019-04879");\n'
             'script_tag(name:"solution", value:"meh");\n'
         ).splitlines()
+
         results = list(
             CheckDescription.run(
                 nasl_file,
