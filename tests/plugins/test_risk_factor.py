8f0b2d3039682ebb657932e953a49a23d7309145
pascalholthaus <pascal.holthaus@greenbone.net>
2022-03-08 18:29:48 +0100

    Init risk_factor

---
+++ tests/plugins/test_risk_factor.py
@@ -0,0 +1,70 @@
+# Copyright (C) 2022 Greenbone Networks GmbH
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from pathlib import Path
+
+from naslinter.plugin import LinterError
+from naslinter.plugins.risk_factor import CheckRiskFactor
+
+from . import PluginTestCase
+
+
+class RiskFactorTestCase(PluginTestCase):
+    def test_ok(self):
+        path = Path(__file__).parent / "test.nasl"
+        content = (
+            'script_tag(name:"cvss_base", value:"4.0");\n'
+            'script_tag(name:"summary", value:"Foo Bar.");\n'
+            'script_tag(name:"solution_type", value:"VendorFix");\n'
+            'script_tag(name:"solution", value:"meh");\n'
+        )
+
+        results = list(
+            CheckRiskFactor.run(
+                path,
+                content,
+                tag_pattern=self.tag_pattern,
+                special_tag_pattern=self.special_tag_pattern,
+            )
+        )
+
+        self.assertEqual(len(results), 0)
+
+    def test_risk_factor(self):
+        path = Path(__file__).parent / "test.nasl"
+        content = (
+            'script_tag(name:"cvss_base", value:"4.0");\n'
+            'script_tag(name:"summary", value:"Foo Bar.");\n'
+            'script_tag(name:"solution_type", value:"VendorFix");\n'
+            'script_tag(name:"risk_factor", value:"TestTest");\n'
+        )
+
+        results = list(
+            CheckRiskFactor.run(
+                path,
+                content,
+                tag_pattern=self.tag_pattern,
+                special_tag_pattern=self.special_tag_pattern,
+            )
+        )
+        self.assertEqual(len(results), 1)
+
+        self.assertIsInstance(results[0], LinterError)
+        self.assertEqual(
+            "Risk factor tag found: TestTest",
+            results[0].message,
+        )
