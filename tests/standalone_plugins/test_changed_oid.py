357a10751cb2402fdba3cfdd60e1cac0457881bf
pascalholthaus <94793111+pascalholthaus@users.noreply.github.com>
2022-05-16 10:36:45 +0200

    Add: Add a standalone Plugin for `ChangedOID` check (#205)
    * Init standalone changed_oid
    
    * First unittest
    
    * Some unittests
    
    * Fix spelling
    
    * Fix spelling
    
    * Fix pip install
    
    * Changed --diff-filter=AM to --diff-filter=d to get any file

---
+++ tests/standalone_plugins/test_changed_oid.py
@@ -0,0 +1,96 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+#
+#  test_changed_oid.py
+#
+# Copyright (C) 2022 Greenbone Networks GmbH
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import os
+import unittest
+import tempfile
+from contextlib import contextmanager
+from pathlib import Path
+from typing import Generator
+from subprocess import SubprocessError
+from troubadix.standalone_plugins.changed_oid import check_oid, git, parse_args
+
+
+@contextmanager
+def tempgitdir() -> Generator[Path, None, None]:
+    cwd = Path.cwd()
+    tempdir = tempfile.TemporaryDirectory()
+    temppath = Path(tempdir.name)
+    os.chdir(str(temppath))
+    git("init", "-b", "main")
+    git("config", "--local", "user.email", "max.mustermann@example.com")
+    git("config", "--local", "user.name", "Max Mustermann")
+    yield temppath
+    tempdir.cleanup()
+    os.chdir(str(cwd))
+
+
+def testgit(tmpdir: Path, ok: bool = False) -> None:
+    test_file = tmpdir / "test.nasl"
+    test_file.write_text('script_oid("1.3.6.1.4.1.25623.1.0.100313");')
+    git("add", str(test_file))
+    git("commit", "-m", "test")
+    git("checkout", "-b", "test")
+    if ok:
+        test_file.write_text(
+            'script_oid("1.3.6.1.4.1.25623.1.0.100313");\ntest'
+        )
+    else:
+        test_file.write_text('script_oid("2.3.6.1.4.1.25623.1.0.100313");')
+    git("add", "-u")
+    git("commit", "-m", "test2")
+
+
+class TestChangedOid(unittest.TestCase):
+    def test_check_oid_ok(self):
+        with tempgitdir() as tmpdir:
+            testgit(tmpdir, True)
+            parsed_args = parse_args(["-c", "main..test"])
+            self.assertFalse(check_oid(parsed_args))
+
+    def test_check_oid_fail(self):
+        with tempgitdir() as tmpdir:
+            testgit(tmpdir)
+            parsed_args = parse_args(["-c", "main..test"])
+            self.assertTrue(check_oid(parsed_args))
+
+    def test_git_fail(self):
+        with self.assertRaises(SubprocessError):
+            git("bla")
+
+    def test_git_ok(self):
+        git("--version")
+
+    def test_args_ok(self):
+        with tempgitdir() as tmpdir:
+            testgit(tmpdir)
+            self.assertEqual(
+                parse_args(["-c", "main..test"]).commit_range, "main..test"
+            )
+            self.assertEqual(
+                parse_args(["-c", "main..test", "-f", "test.nasl"]).files,
+                [Path("test.nasl")],
+            )
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ troubadix/standalone_plugins/changed_oid.py
@@ -0,0 +1,153 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+#
+#  changed_oid.py
+#
+# Copyright (C) 2022 Greenbone Networks GmbH
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import re
+import subprocess
+import sys
+import os
+from argparse import ArgumentParser, Namespace
+from pathlib import Path
+from typing import Iterable
+
+
+def file_type(string: str) -> Path:
+    file_path = Path(string)
+    if not file_path.exists() and not file_path.is_file():
+        raise ValueError(f"{string} is not a file.")
+    return file_path
+
+
+def parse_args(args: Iterable[str]) -> Namespace:
+    parser = ArgumentParser(
+        description="Check for changed oid",
+    )
+    parser.add_argument(
+        "-c",
+        "--commit_range",
+        type=str,
+        required=True,
+        help=(
+            "Git commit range to check e.g "
+            "2c87f4b6062804231fd508411510ca07fd270380^..HEAD or"
+            "YOUR_BRANCH..main"
+        ),
+    )
+    parser.add_argument(
+        "-f",
+        "--files",
+        nargs="+",
+        type=file_type,
+        default=[],
+        help=(
+            "List of files to diff."
+            "If empty use all files added or modifyed in the"
+            " commit range"
+        ),
+    )
+    return parser.parse_args(args=args)
+
+
+def git(*args) -> str:
+    return subprocess.run(
+        ["git"] + list(args), capture_output=True, encoding="utf-8", check=True
+    ).stdout
+
+
+def check_oid(args: Namespace) -> bool:
+    """The script checks (via git diff) if the passed VT has changed the
+    OID in the following tag:
+
+    - script_oid("1.2.3");
+
+    This is only allowed in rare cases (e.g. a single VT was split into
+    two VTs).
+    """
+
+    if not args.files:
+        args.files += [
+            Path(f)
+            for f in git(
+                "diff", "--name-only", "--diff-filter=d", args.commit_range
+            ).splitlines()
+        ]
+
+    rcode = False
+    for nasl_file in args.files:
+        if nasl_file.suffix != ".nasl" or not nasl_file.exists():
+            continue
+
+        print(f"Check file {nasl_file}")
+        text = git(
+            "-c",
+            "color.status=false",
+            "--no-pager",
+            "diff",
+            args.commit_range,
+            nasl_file,
+        )
+
+        oid_added = re.search(
+            r'^\+\s*script_oid\s*\(\s*["\'](?P<oid>[0-9.]+)["\']\s*\)\s*;',
+            text,
+            re.MULTILINE,
+        )
+        if not oid_added or not oid_added.group("oid"):
+            continue
+
+        oid_removed = re.search(
+            r'^-\s*script_oid\s*\(\s*["\'](?P<oid>[0-9.]+)["\']\s*\)\s*;',
+            text,
+            re.MULTILINE,
+        )
+        if not oid_removed or not oid_removed.group("oid"):
+            continue
+
+        if oid_added.group("oid") != oid_removed.group("oid"):
+            print(
+                f"Change OID IN {nasl_file} "
+                f"OID_NEW: {oid_added.group('oid')} "
+                f"OID OLD: {oid_removed.group('oid')}"
+            )
+            rcode = True
+    return rcode
+
+
+def main() -> int:
+    args = sys.argv[1:]
+
+    try:
+        git_base = git("rev-parse", "--show-toplevel")
+        os.chdir(git_base.rstrip("\n"))
+    except subprocess.SubprocessError:
+        print(
+            "Your current working directory doesn't belong to a git repository"
+        )
+        return 1
+
+    if check_oid(parse_args(args)):
+        return 2
+
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
