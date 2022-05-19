57983fc3cadac3bff072a3736203536fa7c7d92b
pascalholthaus <pascal.holthaus@greenbone.net>
2022-05-19 14:21:56 +0200

    Add deprecated_bugtraq_id plugin

---
+++ tests/plugins/test_deprecated_bugtraq_id.py
@@ -0,0 +1,62 @@
+# Copyright (C) 2022 Greenbone Networks GmbH
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from pathlib import Path
+
+from troubadix.plugin import LinterError
+from troubadix.plugins.deprecated_bugtraq_id import CheckDeprecatedBugtraqId
+
+from . import PluginTestCase
+
+
+class CheckDeprecatedBugtraqIdTestCase(PluginTestCase):
+    def test_ok(self):
+        nasl_file = Path(__file__).parent / "test.nasl"
+        content = (
+            'script_tag(name:"cvss_base_vector", value:"AV:N/A:N");\n'
+            'script_tag(name:"summary", value:"Foo Bar.");\n'
+            'script_family("FreeBSD Local Security Checks");\n'
+        )
+        fake_context = self.create_file_plugin_context(
+            nasl_file=nasl_file, file_content=content
+        )
+        plugin = CheckDeprecatedBugtraqId(fake_context)
+
+        results = list(plugin.run())
+
+        self.assertEqual(len(results), 0)
+
+    def test_bugtraq_id(self):
+        nasl_file = Path(__file__).parent / "test.nasl"
+        content = (
+            'script_tag(name:"cvss_base_vector", value:"AV:N/A:N");\n'
+            'script_tag(name:"summary", value:"Foo Bar.");\n'
+            "script_bugtraq_id(00000);\n"
+            'script_family("TestTest");\n'
+        )
+        fake_context = self.create_file_plugin_context(
+            nasl_file=nasl_file, file_content=content
+        )
+        plugin = CheckDeprecatedBugtraqId(fake_context)
+
+        results = list(plugin.run())
+
+        self.assertEqual(len(results), 1)
+        self.assertIsInstance(results[0], LinterError)
+        self.assertEqual(
+            "Found bugtraq id.", results[0].message
+        )
