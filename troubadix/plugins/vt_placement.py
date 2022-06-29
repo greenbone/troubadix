76322a301bc4fad1f4f47a99adcc77f2077ba901
pascalholthaus <94793111+pascalholthaus@users.noreply.github.com>
2022-06-29 08:42:11 +0200

    Add: common, 21.04, 22.04 folder to search path in vt_placement.py (#280)
    * Add common, 21.04, 22.04 folder to search path.
    
    * Move FEED_VERSIONS list to helper module.

--- troubadix/plugins/vt_placement.py
+++ troubadix/plugins/vt_placement.py
@@ -21,7 +21,7 @@ from pathlib import Path
 from typing import Iterator
 
 from troubadix.helper import ScriptTag, SpecialScriptTag, get_script_tag_pattern
-from troubadix.helper.helper import ENTERPRISE_FOLDERS
+from troubadix.helper.helper import ENTERPRISE_FOLDERS, FEED_VERSIONS
 from troubadix.helper.patterns import _get_special_script_tag_pattern
 from troubadix.plugin import FileContentPlugin, LinterError, LinterResult
 
@@ -71,11 +71,17 @@ class CheckVTPlacement(FileContentPlugin):
         if match is not None:
             return
 
-        if root / nasl_file.name == nasl_file:
+        if any(
+            (root / vers / nasl_file.name) == nasl_file
+            for vers in FEED_VERSIONS
+        ):
             return
 
         for folder in chain(["attic"], ENTERPRISE_FOLDERS):
-            if root / folder / nasl_file.name == nasl_file:
+            if any(
+                (root / vers / folder / nasl_file.name) == nasl_file
+                for vers in FEED_VERSIONS
+            ):
                 return
 
         yield LinterError(
