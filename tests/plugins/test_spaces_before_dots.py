a14c471b48ad9c939f8032767bcd2bb98673b5f1
Niklas Hargarter <niklashargarter@gmail.com>
2024-09-12 16:05:38 +0200

    Add: fix method to spaces_before_dots

--- tests/plugins/test_spaces_before_dots.py
+++ tests/plugins/test_spaces_before_dots.py
@@ -4,6 +4,8 @@
 from pathlib import Path
 
 from tests.plugins import PluginTestCase
+from troubadix.helper import CURRENT_ENCODING
+from troubadix.plugin import LinterFix
 from troubadix.plugins.spaces_before_dots import CheckSpacesBeforeDots
 
 
@@ -30,7 +32,7 @@ class TestSpacesBeforeDots(PluginTestCase):
               script_tag(name:"summary", value:"Foo Bar .");
               script_tag(name:"vuldetect", value:"Foo Bar .");
               script_tag(name:"insight", value:"Foo Bar .");
-              script_tag(name:"impact", value:"Foo . Bar");
+              script_tag(name:"impact", value:"Foo . Bar . Foo");
             """
             'script_tag(name:"affected", value:"Foo\n.\nBar.");'
             'script_tag(name:"solution", value:"Foo Bar\n.");'
@@ -40,7 +42,7 @@ class TestSpacesBeforeDots(PluginTestCase):
         )
         plugin = CheckSpacesBeforeDots(fake_context)
         results = list(plugin.run())
-        self.assertEqual(len(results), 6)
+        self.assertEqual(len(results), 7)
         self.assertEqual(
             results[0].message,
             "value of script_tag summary has alteast one occurence of excess"
@@ -58,3 +60,44 @@ class TestSpacesBeforeDots(PluginTestCase):
         results = list(plugin.run())
 
         self.assertEqual(len(results), 0)
+
+    def test_fix(self):
+        with self.create_directory() as tempdir:
+            path = tempdir / "file.nasl"
+            content = (
+                """
+                  script_tag(name:"summary", value:"Foo Bar .");
+                  script_tag(name:"vuldetect", value:"Foo Bar .");
+                  script_tag(name:"insight", value:"Foo Bar .");
+                  script_tag(name:"impact", value:"Foo . Bar . Foo");
+                """
+                'script_tag(name:"affected", value:"Foo\n.\nBar.");'
+                'script_tag(name:"solution", value:"Foo Bar\n.");'
+            )
+            expected_modified_content = (
+                """
+                  script_tag(name:"summary", value:"Foo Bar.");
+                  script_tag(name:"vuldetect", value:"Foo Bar.");
+                  script_tag(name:"insight", value:"Foo Bar.");
+                  script_tag(name:"impact", value:"Foo. Bar. Foo");
+                """
+                'script_tag(name:"affected", value:"Foo.\nBar.");'
+                'script_tag(name:"solution", value:"Foo Bar.");'
+            )
+            path.write_text(content, encoding=CURRENT_ENCODING)
+
+            fake_context = self.create_file_plugin_context(
+                nasl_file=path, file_content=content
+            )
+
+            plugin = CheckSpacesBeforeDots(fake_context)
+
+            # keep list() to consume the iterator
+            list(plugin.run())
+
+            results = list(plugin.fix())
+            self.assertEqual(len(results), 1)
+
+            self.assertIsInstance(results[0], LinterFix)
+            modified_content = path.read_text(encoding=CURRENT_ENCODING)
+            self.assertEqual(modified_content, expected_modified_content)
