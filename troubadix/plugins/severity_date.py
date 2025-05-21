d2c0f57f5333b3b7184c008386658252ac13e763
Philipp Brozmann <philipp.brozmann@greenbone.net>
2025-05-21 15:19:25 +0200

    Change: Use helper function to check severity_date

--- troubadix/plugins/severity_date.py
+++ troubadix/plugins/severity_date.py
@@ -6,6 +6,7 @@ from pathlib import Path
 from typing import Iterator
 
 from troubadix.helper import ScriptTag, get_script_tag_pattern
+from troubadix.helper.date_format import check_date
 from troubadix.plugin import FileContentPlugin, LinterError, LinterResult
 
 
@@ -23,67 +24,49 @@ class CheckSeverityDate(FileContentPlugin):
         ):
             return
 
-        if "severity_date" not in file_content:
-            return
-
-        tag_pattern = get_script_tag_pattern(ScriptTag.SEVERITY_DATE)
-
-        # Example: "2017-11-29 13:56:41 +0100 (Wed, 29 Nov 2017)"
-        match = tag_pattern.search(file_content)
-
-        if not match:
-            yield LinterError(
-                "False or incorrectly formatted severity_date.",
-                file=nasl_file,
-                plugin=self.name,
-            )
-            return
-
-        try:
-            date_left = datetime.strptime(
-                match.group("value")[:25], "%Y-%m-%d %H:%M:%S %z"
-            )
-            # 2017-11-29 13:56:41 +0100 (error if no timezone)
-            date_right = datetime.strptime(
-                match.group("value")[27:43], "%a, %d %b %Y"
-            )
-            week_day_parsed = date_right.strftime("%a")
-        except ValueError:
-            yield LinterError(
-                "False or incorrectly formatted severity_date.",
-                file=nasl_file,
-                plugin=self.name,
-            )
-            return
-
-        week_day_str = match.group("value")[27:30]
-        # Wed, 29 Nov 2017
-        if date_left.date() != date_right.date():
-            yield LinterError(
-                "The severity_date consists of two different dates.",
-                file=nasl_file,
-                plugin=self.name,
-            )
-        # Check correct weekday
-        elif week_day_str != week_day_parsed:
-            formatted_date = week_day_parsed
-            yield LinterError(
-                f"Wrong day of week. Please change it from '{week_day_str}"
-                f"' to '{formatted_date}'.",
-                file=nasl_file,
-                plugin=self.name,
-            )
-
+        severity_date_pattern = get_script_tag_pattern(ScriptTag.SEVERITY_DATE)
         last_modification_pattern = get_script_tag_pattern(
             ScriptTag.LAST_MODIFICATION
         )
-        if match := last_modification_pattern.search(file_content):
-            last_modification = datetime.strptime(
-                match.group("value")[:25], "%Y-%m-%d %H:%M:%S %z"
+
+        # Check severity date if available
+        if match_severity_date := severity_date_pattern.search(file_content):
+            yield from check_date(
+                match_severity_date.group("value"),
+                "severity_date",
+                nasl_file,
+                self.name,
             )
-            if date_left > last_modification:
-                yield LinterError(
-                    "The severity_date must not be greater than the last modification date.",
-                    file=nasl_file,
-                    plugin=self.name,
+        else:
+            return
+
+        # Check last modification date if available
+        if match_last_mod_date := last_modification_pattern.search(
+            file_content
+        ):
+
+            yield from check_date(
+                match_last_mod_date.group("value"),
+                "last_modification",
+                nasl_file,
+                self.name,
+            )
+
+            try:
+                severity_date = datetime.strptime(
+                    match_severity_date.group("value")[:25],
+                    "%Y-%m-%d %H:%M:%S %z",
                 )
+                last_modification_date = datetime.strptime(
+                    match_last_mod_date.group("value")[:25],
+                    "%Y-%m-%d %H:%M:%S %z",
+                )
+                if severity_date > last_modification_date:
+                    yield LinterError(
+                        "The severity date must not be greater than the "
+                        "last modification date.",
+                        file=nasl_file,
+                        plugin=self.name,
+                    )
+            except Exception:
+                pass
