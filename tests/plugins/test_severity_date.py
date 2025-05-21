c91d0a0a350f5e5294ef7124a129ef3cb3d802ea
Philipp Brozmann <philipp.brozmann@greenbone.net>
2025-05-21 15:19:25 +0200

    Change: Use helper function to check severity_date

--- tests/plugins/test_severity_date.py
+++ tests/plugins/test_severity_date.py
@@ -2,7 +2,6 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from pathlib import Path
-from unittest.mock import MagicMock
 
 from troubadix.plugin import LinterError
 from troubadix.plugins.severity_date import CheckSeverityDate
@@ -35,7 +34,7 @@ class CheckSeverityDateTestCase(PluginTestCase):
 
         self.assertEqual(len(results), 0)
 
-    def test_missing(self):
+    def test_missing_severity_date(self):
         path = Path("some/file.nasl")
         content = ""
         fake_context = self.create_file_plugin_context(
@@ -46,90 +45,6 @@ class CheckSeverityDateTestCase(PluginTestCase):
         results = list(plugin.run())
 
         self.assertEqual(len(results), 0)
-
-    def test_wrong_weekday(self):
-        path = Path("some/file.nasl")
-        content = (
-            '  script_tag(name:"severity_date", value:"2013-05-14 11:24:55 '
-            '+0200 (Mon, 14 May 2013)");\n'
-        )
-        fake_context = self.create_file_plugin_context(
-            nasl_file=path, file_content=content
-        )
-        plugin = CheckSeverityDate(fake_context)
-
-        results = list(plugin.run())
-
-        self.assertEqual(len(results), 1)
-
-        self.assertIsInstance(results[0], LinterError)
-        self.assertEqual(
-            "Wrong day of week. Please change it from 'Mon' to 'Tue'.",
-            results[0].message,
-        )
-
-    def test_no_timezone(self):
-        path = Path("some/file.nasl")
-        content = (
-            '  script_tag(name:"severity_date", value:"2013-05-14 11:24:55 '
-            '(Tue, 14 May 2013)");\n'
-        )
-        fake_context = self.create_file_plugin_context(
-            nasl_file=path, file_content=content
-        )
-        plugin = CheckSeverityDate(fake_context)
-
-        results = list(plugin.run())
-
-        self.assertEqual(len(results), 1)
-
-        self.assertIsInstance(results[0], LinterError)
-        self.assertEqual(
-            "False or incorrectly formatted severity_date.",
-            results[0].message,
-        )
-
-    def test_different_dates(self):
-        path = Path("some/file.nasl")
-        content = (
-            '  script_tag(name:"severity_date", value:"2013-05-14 11:24:55 '
-            '+0200 (Tue, 15 May 2013)");\n'
-        )
-        fake_context = self.create_file_plugin_context(
-            nasl_file=path, file_content=content
-        )
-        plugin = CheckSeverityDate(fake_context)
-
-        results = list(plugin.run())
-
-        self.assertEqual(len(results), 1)
-
-        self.assertIsInstance(results[0], LinterError)
-        self.assertEqual(
-            "The severity_date consists of two different dates.",
-            results[0].message,
-        )
-
-    def test_wrong_length(self):
-        path = Path("some/file.nasl")
-        content = (
-            '  script_tag(name:"severity_date", value:"2013-05-14 11:24:55 '
-            '+0200 (Tue, 14 May 2013 )");\n'
-        )
-        fake_context = MagicMock()
-        fake_context.nasl_file = path
-        fake_context.file_content = content
-        plugin = CheckSeverityDate(fake_context)
-
-        results = list(plugin.run())
-
-        self.assertEqual(len(results), 1)
-
-        self.assertIsInstance(results[0], LinterError)
-        self.assertEqual(
-            "False or incorrectly formatted severity_date.",
-            results[0].message,
-        )
 
     def test_severity_date_greater_than_last_modification(self):
         path = Path("some/file.nasl")
@@ -150,7 +65,7 @@ class CheckSeverityDateTestCase(PluginTestCase):
 
         self.assertIsInstance(results[0], LinterError)
         self.assertEqual(
-            "The severity_date must not be greater than the last modification date.",
+            "The severity date must not be greater than the last modification date.",
             results[0].message,
         )
 
@@ -170,66 +85,3 @@ class CheckSeverityDateTestCase(PluginTestCase):
         results = list(plugin.run())
 
         self.assertEqual(len(results), 0)
-
-    def test_malformed_hour(self):
-        path = Path("some/file.nasl")
-        content = (
-            '  script_tag(name:"severity_date", value:"2013-05-14 111:24:55 '
-            '+0200 (Tue, 14 May 2013)");\n'
-        )
-        fake_context = MagicMock()
-        fake_context.nasl_file = path
-        fake_context.file_content = content
-        plugin = CheckSeverityDate(fake_context)
-
-        results = list(plugin.run())
-
-        self.assertEqual(len(results), 1)
-
-        self.assertIsInstance(results[0], LinterError)
-        self.assertEqual(
-            "False or incorrectly formatted severity_date.",
-            results[0].message,
-        )
-
-    def test_malformed_second(self):
-        path = Path("some/file.nasl")
-        content = (
-            '  script_tag(name:"severity_date", value:"2013-05-14 11:24:55s '
-            '+0200 (Tue, 14 May 2013)");\n'
-        )
-        fake_context = MagicMock()
-        fake_context.nasl_file = path
-        fake_context.file_content = content
-        plugin = CheckSeverityDate(fake_context)
-
-        results = list(plugin.run())
-
-        self.assertEqual(len(results), 1)
-
-        self.assertIsInstance(results[0], LinterError)
-        self.assertEqual(
-            "False or incorrectly formatted severity_date.",
-            results[0].message,
-        )
-
-    def test_malformed_day(self):
-        path = Path("some/file.nasl")
-        content = (
-            '  script_tag(name:"severity_date", value:"2013-05-14D 11:24:55 '
-            '+0200 (Tue, 14 May 2013)");\n'
-        )
-        fake_context = MagicMock()
-        fake_context.nasl_file = path
-        fake_context.file_content = content
-        plugin = CheckSeverityDate(fake_context)
-
-        results = list(plugin.run())
-
-        self.assertEqual(len(results), 1)
-
-        self.assertIsInstance(results[0], LinterError)
-        self.assertEqual(
-            "False or incorrectly formatted severity_date.",
-            results[0].message,
-        )
