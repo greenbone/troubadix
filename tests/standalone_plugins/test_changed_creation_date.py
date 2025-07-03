9db8ce0a349728b79b4a0408ad6645a744c5ff1d
Philipp Brozmann <philipp.brozmann@greenbone.net>
2025-07-03 13:33:04 +0200

    Change: Refactor unit test

--- tests/standalone_plugins/test_changed_creation_date.py
+++ tests/standalone_plugins/test_changed_creation_date.py
@@ -2,241 +2,160 @@
 # SPDX-FileCopyrightText: 2025 Greenbone AG
 
 import unittest
+from argparse import Namespace
 from os import chdir
 from pathlib import Path
 from subprocess import SubprocessError
 from tempfile import TemporaryDirectory
+from unittest.mock import patch
 
 from troubadix.standalone_plugins.changed_creation_date import (
     check_changed_creation_date,
     git,
     main,
-    parse_args,
+    parse_arguments,
 )
-from troubadix.standalone_plugins.util import temporary_git_directory
+
+TEST_ARGS = Namespace(commit_range="main..test", files=[Path("test.nasl")])
 
 
 class TestChangedCreationDate(unittest.TestCase):
 
-    def test_check_creation_date_ok(self):
-        with temporary_git_directory() as tmpdir:
+    @patch("troubadix.standalone_plugins.changed_creation_date.Path.exists")
+    @patch("troubadix.standalone_plugins.changed_creation_date.git")
+    def test_check_creation_date_ok(self, mock_git, mock_exists):
 
-            test_file = tmpdir / "test.nasl"
-            test_file.write_text(
-                'script_tag(name:"creation_date", '
-                'value:"2025-03-04 10:00:00 +0200 (Tue, 04 Mar 2025)");'
-            )
-            git("add", str(test_file))
-            git("commit", "-m", "test")
-            git("checkout", "-b", "test")
-            test_file.write_text(
-                'script_tag(name:"creation_date", '
-                'value:"2025-03-04 10:00:00 +0200 (Tue, 04 Mar 2025)");\ntest'
-            )
-            git("add", "-u")
-            git("commit", "-m", "test2")
+        mock_git.return_value = (
+            '-script_tag(name:"creation_date",'
+            ' value:"2025-03-04 10:00:00 +0200 (Tue, 04 Mar 2025)");\n'
+            '+script_tag(name:"creation_date",'
+            ' value:"2025-03-04 10:00:00 +0200 (Tue, 04 Mar 2025)");\n'
+            "+test"
+        )
+        mock_exists.return_value = True
 
-            parsed_args = parse_args(["-c", "main..test"])
-            self.assertFalse(check_changed_creation_date(parsed_args))
+        self.assertFalse(check_changed_creation_date(TEST_ARGS))
 
-    def test_check_creation_date_fail(self):
-        with temporary_git_directory() as tmpdir:
+    @patch("troubadix.standalone_plugins.changed_creation_date.Path.exists")
+    @patch("troubadix.standalone_plugins.changed_creation_date.git")
+    def test_check_creation_date_fail(self, mock_git, mock_exists):
 
-            test_file = tmpdir / "test.nasl"
-            test_file.write_text(
-                'script_tag(name:"creation_date", '
-                'value:"2025-03-04 10:00:00 +0200 (Tue, 04 Mar 2025)");'
-            )
-            git("add", str(test_file))
-            git("commit", "-m", "test")
-            git("checkout", "-b", "test")
-            test_file.write_text(
-                'script_tag(name:"creation_date", '
-                'value:"2020-03-04 10:00:00 +0200 (Wed, 04 Mar 2020)");'
-            )
-            git("add", "-u")
-            git("commit", "-m", "test2")
+        mock_git.return_value = (
+            '-script_tag(name:"creation_date",'
+            ' value:"2025-03-04 10:00:00 +0200 (Tue, 04 Mar 2025)");\n'
+            '+script_tag(name:"creation_date",'
+            ' value:"2020-03-04 10:00:00 +0200 (Wed, 04 Mar 2020)");'
+        )
+        mock_exists.return_value = True
 
-            parsed_args = parse_args(["-c", "main..test"])
-            self.assertTrue(check_changed_creation_date(parsed_args))
+        self.assertTrue(check_changed_creation_date(TEST_ARGS))
 
-    def test_not_nasl_file(self):
-        with temporary_git_directory() as tmpdir:
+    @patch("troubadix.standalone_plugins.changed_creation_date.Path.exists")
+    @patch("troubadix.standalone_plugins.changed_creation_date.git")
+    def test_creation_date_not_modified_lines_added(
+        self, mock_git, mock_exists
+    ):
 
-            test_file = tmpdir / "test.txt"
-            test_file.write_text(
-                'script_tag(name:"creation_date", '
-                'value:"2025-03-04 10:00:00 +0200 (Tue, 04 Mar 2025)");'
-            )
-            git("add", str(test_file))
-            git("commit", "-m", "test")
-            git("checkout", "-b", "test")
-            test_file.write_text(
-                'script_tag(name:"creation_date", '
-                'value:"2025-03-04 10:00:00 +0200 (Tue, 04 Mar 2025)");\ntest'
-            )
-            git("add", "-u")
-            git("commit", "-m", "test2")
+        mock_git.return_value = (
+            '-script_tag(name:"creation_date",'
+            ' value:"2025-03-04 10:00:00 +0200 (Tue, 04 Mar 2025)");\n'
+            "test\n"
+            '+script_tag(name:"creation_date",'
+            ' value:"2025-03-04 10:00:00 +0200 (Tue, 04 Mar 2025)");'
+        )
+        mock_exists.return_value = True
 
-            parsed_args = parse_args(["-c", "main..test"])
-            self.assertFalse(check_changed_creation_date(parsed_args))
+        self.assertFalse(check_changed_creation_date(TEST_ARGS))
 
-    def test_creation_date_not_modified_lines_added(self):
-        with temporary_git_directory() as tmpdir:
+    @patch("troubadix.standalone_plugins.changed_creation_date.Path.exists")
+    @patch("troubadix.standalone_plugins.changed_creation_date.git")
+    def test_creation_date_not_modified_lines_removed(
+        self, mock_git, mock_exists
+    ):
 
-            test_file = tmpdir / "test.nasl"
-            test_file.write_text(
-                'script_tag(name:"creation_date", '
-                'value:"2025-03-04 10:00:00 +0200 (Tue, 04 Mar 2025)");'
-            )
-            git("add", str(test_file))
-            git("commit", "-m", "test")
-            git("checkout", "-b", "test")
-            test_file.write_text(
-                "test\n"
-                'script_tag(name:"creation_date", '
-                'value:"2025-03-04 10:00:00 +0200 (Tue, 04 Mar 2025)");'
-            )
-            git("add", "-u")
-            git("commit", "-m", "test2")
+        mock_git.return_value = (
+            '-script_tag(name:"This got removed", value:"Nothing");'
+        )
+        mock_exists.return_value = True
 
-            parsed_args = parse_args(["-c", "main..test"])
-            self.assertFalse(check_changed_creation_date(parsed_args))
+        self.assertFalse(check_changed_creation_date(TEST_ARGS))
 
-    def test_creation_date_not_modified_lines_removed(self):
-        with temporary_git_directory() as tmpdir:
+    @patch("troubadix.standalone_plugins.changed_creation_date.Path.exists")
+    @patch("troubadix.standalone_plugins.changed_creation_date.git")
+    def test_creation_date_added_not_removed(self, mock_git, mock_exists):
 
-            test_file = tmpdir / "test.nasl"
-            test_file.write_text(
-                'script_tag(name:"creation_date", '
-                'value:"2025-03-04 10:00:00 +0200 (Tue, 04 Mar 2025)");\n'
-                "test"
-            )
-            git("add", str(test_file))
-            git("commit", "-m", "test")
-            git("checkout", "-b", "test")
-            test_file.write_text(
-                'script_tag(name:"creation_date", '
-                'value:"2025-03-04 10:00:00 +0200 (Tue, 04 Mar 2025)");\n'
-            )
-            git("add", "-u")
-            git("commit", "-m", "test2")
+        mock_git.return_value = (
+            '+script_tag(name:"This got added", value:"Something");\n'
+            '+script_tag(name:"creation_date", '
+            ' value:"2025-03-04 10:00:00 +0200 (Tue, 04 Mar 2025)");'
+        )
+        mock_exists.return_value = True
 
-            parsed_args = parse_args(["-c", "main..test"])
-            self.assertFalse(check_changed_creation_date(parsed_args))
-
-    def test_creation_date_added_not_removed(self):
-        with temporary_git_directory() as tmpdir:
-
-            test_file = tmpdir / "test.nasl"
-            test_file.write_text("test")
-            git("add", str(test_file))
-            git("commit", "-m", "test")
-            git("checkout", "-b", "test")
-            test_file.write_text(
-                "test\n"
-                'script_tag(name:"creation_date", '
-                'value:"2025-03-04 10:00:00 +0200 (Tue, 04 Mar 2025)");'
-            )
-            git("add", "-u")
-            git("commit", "-m", "test2")
-
-            parsed_args = parse_args(["-c", "main..test"])
-            self.assertFalse(check_changed_creation_date(parsed_args))
+        self.assertFalse(check_changed_creation_date(TEST_ARGS))
 
     def test_git_fail(self):
         with self.assertRaises(SubprocessError):
-            git("bla")
+            git("blub")
 
     def test_git_ok(self):
         git("--version")
 
-    def test_args_ok(self):
-        with temporary_git_directory() as tmpdir:
+    @patch("troubadix.standalone_plugins.changed_creation_date.git")
+    @patch(
+        "troubadix.standalone_plugins.changed_creation_date.ArgumentParser.parse_args"
+    )
+    def test_args_ok(self, mock_parse_args, mock_git):
 
-            test_file = tmpdir / "test.nasl"
-            test_file.write_text(
-                'script_tag(name:"creation_date", '
-                'value:"2025-03-04 10:00:00 +0200 (Tue, 04 Mar 2025)");'
-            )
-            git("add", str(test_file))
-            git("commit", "-m", "test")
-            git("checkout", "-b", "test")
-            test_file.write_text(
-                'script_tag(name:"creation_date", '
-                'value:"2020-03-04 10:00:00 +0200 (Wed, 04 Mar 2020)");'
-            )
-            git("add", "-u")
-            git("commit", "-m", "test2")
+        mock_parse_args.return_value = Namespace(
+            commit_range="main..test", files=[]
+        )
+        mock_git.return_value = "test1.nasl\ntest2.nasl\ntest3.txt"
 
-            self.assertEqual(
-                parse_args(["-c", "main..test"]).commit_range, "main..test"
-            )
-            self.assertEqual(
-                parse_args(["-c", "main..test", "-f", "test.nasl"]).files,
-                [Path("test.nasl")],
-            )
+        parsed_args = parse_arguments()
+
+        self.assertEqual(parsed_args.commit_range, "main..test")
+        self.assertEqual(
+            parsed_args.files, [Path("test1.nasl"), Path("test2.nasl")]
+        )
 
     def test_main_no_git_repository(self):
         cwd = Path.cwd()
         with TemporaryDirectory() as tempdir:
             try:
                 chdir(tempdir)
-                with unittest.mock.patch(
-                    "sys.argv",
-                    ["troubadix-changed-creation-date", "-c", "main..test"],
-                ):
-                    self.assertEqual(main(), 1)
+                self.assertEqual(main(), 1)
             finally:
                 chdir(cwd)
 
-    def test_main_check_creation_date_ok(self):
-        with temporary_git_directory() as tmpdir:
+    @patch("troubadix.standalone_plugins.changed_creation_date.parse_arguments")
+    @patch(
+        "troubadix.standalone_plugins.changed_creation_date.check_changed_creation_date"
+    )
+    @patch("troubadix.standalone_plugins.changed_creation_date.os.chdir")
+    @patch("troubadix.standalone_plugins.changed_creation_date.git")
+    def test_main_check_creation_date_ok(
+        self, mock_git, _, mock_check_creation_date, __
+    ):
 
-            test_file = tmpdir / "test.nasl"
-            test_file.write_text(
-                'script_tag(name:"creation_date", '
-                'value:"2025-03-04 10:00:00 +0200 (Tue, 04 Mar 2025)");'
-            )
-            git("add", str(test_file))
-            git("commit", "-m", "test")
-            git("checkout", "-b", "test")
-            test_file.write_text(
-                'script_tag(name:"creation_date", '
-                'value:"2025-03-04 10:00:00 +0200 (Tue, 04 Mar 2025)");\ntest'
-            )
-            git("add", "-u")
-            git("commit", "-m", "test2")
+        mock_git.return_value = "test_directory"
+        mock_check_creation_date.return_value = 0
 
-            with unittest.mock.patch(
-                "sys.argv",
-                ["troubadix-changed-creation-date", "-c", "main..test"],
-            ):
-                self.assertEqual(main(), 0)
+        self.assertEqual(main(), 0)
 
-    def test_main_check_creation_date_fail(self):
-        with temporary_git_directory() as tmpdir:
+    @patch("troubadix.standalone_plugins.changed_creation_date.parse_arguments")
+    @patch(
+        "troubadix.standalone_plugins.changed_creation_date.check_changed_creation_date"
+    )
+    @patch("troubadix.standalone_plugins.changed_creation_date.os.chdir")
+    @patch("troubadix.standalone_plugins.changed_creation_date.git")
+    def test_main_check_creation_date_fail(
+        self, mock_git, _, mock_check_creation_date, __
+    ):
 
-            test_file = tmpdir / "test.nasl"
-            test_file.write_text(
-                'script_tag(name:"creation_date", '
-                'value:"2025-03-04 10:00:00 +0200 (Tue, 04 Mar 2025)");'
-            )
-            git("add", str(test_file))
-            git("commit", "-m", "test")
-            git("checkout", "-b", "test")
-            test_file.write_text(
-                'script_tag(name:"creation_date", '
-                'value:"2020-03-04 10:00:00 +0200 (Wed, 04 Mar 2020)");'
-            )
-            git("add", "-u")
-            git("commit", "-m", "test2")
+        mock_git.return_value = "test_directory"
+        mock_check_creation_date.return_value = 2
 
-            with unittest.mock.patch(
-                "sys.argv",
-                ["troubadix-changed-creation-date", "-c", "main..test"],
-            ):
-                self.assertEqual(main(), 2)
+        self.assertEqual(main(), 2)
 
 
 if __name__ == "__main__":
