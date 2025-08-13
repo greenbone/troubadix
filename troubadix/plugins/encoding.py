313848f6c5920deeedfa70df4dfd7377676cf46e
mbrinkhoff <99736009+mbrinkhoff@users.noreply.github.com>
2025-08-13 15:36:14 +0200

    Revert "Change: encoding test cases after plugin rework"
    This reverts commit f78a74745556946972a50660cb9ffbe06a4d6ac6.

--- troubadix/plugins/encoding.py
+++ troubadix/plugins/encoding.py
@@ -32,7 +32,8 @@ class CheckEncoding(FilePlugin):
             raw = f.read()
 
         # Use magic to detect encoding
-        detected_encoding = magic.Magic(mime_encoding=True).from_buffer(raw)
+        m = magic.Magic(mime_encoding=True)
+        detected_encoding = m.from_buffer(raw)
 
         if detected_encoding not in ALLOWED_ENCODINGS:
             yield LinterError(
