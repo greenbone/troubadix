ae3344b8629697678bde05617741ad1413eac4c9
Niklas Hargarter <niklashargarter@gmail.com>
2025-10-24 10:05:59 +0200

    Change: simplify script_add_preference_id

--- troubadix/plugins/script_add_preference_id.py
+++ troubadix/plugins/script_add_preference_id.py
@@ -35,7 +35,6 @@ class CheckScriptAddPreferenceId(FileContentPlugin):
         ).finditer(file_content)
 
         seen = set()
-        duplicates = set()
 
         # Secondary id regex
         for pref_match in preferences_matches:
@@ -46,13 +45,10 @@ class CheckScriptAddPreferenceId(FileContentPlugin):
 
             pref_id = id_match.group("id")
             if pref_id in seen:
-                duplicates.add(pref_id)
-            else:
-                seen.add(pref_id)
 
-        for pref_id in duplicates:
-            yield LinterError(
-                f"script_add_preference id {pref_id} is used multiple times",
-                file=nasl_file,
-                plugin=self.name,
-            )
+                yield LinterError(
+                    f"script_add_preference id {pref_id} is used multiple times",
+                    file=nasl_file,
+                    plugin=self.name,
+                )
+            seen.add(pref_id)
