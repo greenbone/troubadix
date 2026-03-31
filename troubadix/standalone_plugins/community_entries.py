11171dd9ef80d80a1c376bd49acaf39205e624e9
mbrinkhoff <99736009+mbrinkhoff@users.noreply.github.com>
2026-03-31 11:12:38 +0200

    VTOPS-356: Detect community entry additions (#935)
    * Add community entries standalone
    
    * Add: Unittests for community_entries
    
    * Add: pyproject scripts entry
    
    * Fix: Typo
    
    Co-authored-by: Amy S. <159793890+amy-gb@users.noreply.github.com>
    
    * Fix: if guard for arguments directory usage
    
    ---------
    
    Co-authored-by: Amy S. <159793890+amy-gb@users.noreply.github.com>

---
+++ troubadix/standalone_plugins/community_entries.py
@@ -0,0 +1,78 @@
+# SPDX-FileCopyrightText: 2026 Greenbone AG
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+
+import os
+from argparse import ArgumentParser, Namespace
+
+from troubadix.standalone_plugins.common import git
+
+
+def parse_arguments() -> Namespace:
+    parser = ArgumentParser(
+        "Tool to check for files which would be newly added to the community part of the VTS repo"
+    )
+
+    parser.add_argument("ref_a", help="The ref to diff from")
+
+    parser.add_argument("ref_b", help="The ref to diff to")
+
+    parser.add_argument(
+        "-d", "--directory", help="The directory containing the repository to work in"
+    )
+
+    parser.add_argument(
+        "--required-infix",
+        dest="required_infixes",
+        type=str,
+        nargs="+",
+        default=["/gsf/"],
+        help="Infix required to avoid reporting",
+    )
+
+    parser.add_argument(
+        "--exclude-prefix",
+        dest="excluded_prefixes",
+        type=str,
+        nargs="+",
+        default=["nasl/21.04"],
+        help="Prefix to exclude from reporting",
+    )
+
+    return parser.parse_args()
+
+
+def is_file_excluded(file: str, excludes: list[str]) -> bool:
+    return any(True for exclude in excludes if file.startswith(exclude))
+
+
+def is_infix_present(file: str, infixes: list[str]) -> bool:
+    return any(True for infix in infixes if infix in file)
+
+
+def main():
+
+    arguments = parse_arguments()
+
+    if arguments.directory:
+        os.chdir(arguments.directory)
+
+    files = git(
+        "diff", arguments.ref_b, arguments.ref_a, "--name-only", "--diff-filter=A", "--no-renames"
+    ).splitlines()
+
+    valid_files = (
+        file for file in files if not is_file_excluded(file, arguments.excluded_prefixes)
+    )
+
+    report_files = (
+        file for file in valid_files if not is_infix_present(file, arguments.required_infixes)
+    )
+
+    for file in report_files:
+        print(file)
+
+
+if __name__ == "__main__":
+    main()
