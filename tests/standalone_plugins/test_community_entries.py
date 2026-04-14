99f0a33f01b09c225587fb4aadb5be3ad3ad7a6c
mbrinkhoff <maximilian.brinkhoff@greenbone.net>
2026-04-14 14:34:43 +0200

    Add: Changes for community-entries tool

--- tests/standalone_plugins/test_community_entries.py
+++ tests/standalone_plugins/test_community_entries.py
@@ -4,20 +4,24 @@
 import unittest
 
 from troubadix.standalone_plugins.community_entries import (
-    is_file_excluded,
+    is_file_excluded_by_prefix,
     is_infix_present,
 )
 
 
 class CommunityEntriesTestCase(unittest.TestCase):
     def test_is_file_excluded_with_matching_prefix(self):
-        self.assertTrue(is_file_excluded("nasl/21.04/script.nasl", ["nasl/21.04", "nasl/22.04"]))
+        self.assertTrue(
+            is_file_excluded_by_prefix("nasl/21.04/script.nasl", ["nasl/21.04", "nasl/22.04"])
+        )
 
     def test_is_file_excluded_without_matching_prefix(self):
-        self.assertFalse(is_file_excluded("nasl/common/script.nasl", ["nasl/21.04", "nasl/22.04"]))
+        self.assertFalse(
+            is_file_excluded_by_prefix("nasl/common/script.nasl", ["nasl/21.04", "nasl/22.04"])
+        )
 
     def test_is_file_excluded_with_empty_excludes(self):
-        self.assertFalse(is_file_excluded("nasl/21.04/script.nasl", []))
+        self.assertFalse(is_file_excluded_by_prefix("nasl/21.04/script.nasl", []))
 
     def test_is_infix_present_with_matching_infix(self):
         self.assertTrue(
--- troubadix/standalone_plugins/community_entries.py
+++ troubadix/standalone_plugins/community_entries.py
@@ -4,19 +4,20 @@
 
 
 import os
-from argparse import ArgumentParser, Namespace
+from argparse import ArgumentDefaultsHelpFormatter, ArgumentParser, Namespace
 
 from troubadix.standalone_plugins.common import git
 
 
 def parse_arguments() -> Namespace:
     parser = ArgumentParser(
-        "Tool to check for files which would be newly added to the community part of the VTS repo"
+        "Tool to check for files which would be newly added to the community part of the VTS repo",
+        formatter_class=ArgumentDefaultsHelpFormatter,
     )
 
-    parser.add_argument("ref_a", help="The ref to diff from")
+    parser.add_argument("ref_from", help="The ref to diff from. Example: 'HEAD'")
 
-    parser.add_argument("ref_b", help="The ref to diff to")
+    parser.add_argument("ref_to", help="The ref to diff to. Example: 'main'")
 
     parser.add_argument(
         "-d", "--directory", help="The directory containing the repository to work in"
@@ -26,7 +27,7 @@ def parse_arguments() -> Namespace:
         "--required-infix",
         dest="required_infixes",
         type=str,
-        nargs="+",
+        nargs="*",
         default=["/gsf/"],
         help="Infix required to avoid reporting",
     )
@@ -35,15 +36,24 @@ def parse_arguments() -> Namespace:
         "--exclude-prefix",
         dest="excluded_prefixes",
         type=str,
-        nargs="+",
-        default=["nasl/21.04"],
+        nargs="*",
+        default=["nasl/21.04", "nasl/22.04"],
         help="Prefix to exclude from reporting",
+    )
+
+    parser.add_argument(
+        "--allowed-extension",
+        dest="allowed_extensions",
+        type=str,
+        nargs="*",
+        default=["nasl", "inc"],
+        help="File extensions required for reporting",
     )
 
     return parser.parse_args()
 
 
-def is_file_excluded(file: str, excludes: list[str]) -> bool:
+def is_file_excluded_by_prefix(file: str, excludes: list[str]) -> bool:
     return any(True for exclude in excludes if file.startswith(exclude))
 
 
@@ -51,19 +61,31 @@ def is_infix_present(file: str, infixes: list[str]) -> bool:
     return any(True for infix in infixes if infix in file)
 
 
-def main():
+def execute_git_diff(arguments: Namespace) -> list[str]:
+    file_extension_wildcards = [f"*.{extension}" for extension in arguments.allowed_extensions]
 
+    return git(
+        "diff",
+        arguments.ref_to,
+        arguments.ref_from,
+        "--name-only",
+        "--diff-filter=A",
+        "--no-renames",
+        "--",
+        *file_extension_wildcards,
+    ).splitlines()
+
+
+def main():
     arguments = parse_arguments()
 
     if arguments.directory:
         os.chdir(arguments.directory)
 
-    files = git(
-        "diff", arguments.ref_b, arguments.ref_a, "--name-only", "--diff-filter=A", "--no-renames"
-    ).splitlines()
+    files = execute_git_diff(arguments)
 
     valid_files = (
-        file for file in files if not is_file_excluded(file, arguments.excluded_prefixes)
+        file for file in files if not is_file_excluded_by_prefix(file, arguments.excluded_prefixes)
     )
 
     report_files = (
