2233d9c1c5ece256b85995ac255658fa3e88c1e8
Amy Schools <amy.schools@greenbone.net>
2026-08-24 12:24:34 +0200

    black reformatting

--- troubadix/standalone_plugins/malicious_nasl_check.py
+++ troubadix/standalone_plugins/malicious_nasl_check.py
@@ -67,7 +67,11 @@ def parse_arguments():
     parser = argparse.ArgumentParser("Check for unexpected files or commands in NASL scripts.")
 
     parser.add_argument(
-        "-d", "--vt-dir", default=DEFAULT_VTS_DIR, type=Path, help="Root location of the VTS repository"
+        "-d",
+        "--vt-dir",
+        default=DEFAULT_VTS_DIR,
+        type=Path,
+        help="Root location of the VTS repository",
     )
 
     return parser.parse_args()
